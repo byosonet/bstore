426b09ba9584af78e001508fc5633624fa8e20c2
gtrejo <gtrejo.armenta@gmail.com>
2016-06-03 20:38:14 -0500

    add test hibernate

---
+++ bstore-web/src/test/java/com/bstore/services/test/TestHibernate.java
@@ -0,0 +1,27 @@
+package com.bstore.services.test;
+
+/**
+ *
+ * @author Priscila
+ */
+import com.bstore.services.persistence.dao.UsuarioDaoImpl;
+import com.bstore.services.persistence.hbm.Usuario;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+ 
+public class TestHibernate {
+ 
+    public static void main(String[] args) throws Exception
+    {
+    	System.out.println("--Test::");
+        ApplicationContext context = new FileSystemXmlApplicationContext("classpath:com/bstore/services/test/ContextHibernate.xml");
+        UsuarioDaoImpl usuario = (UsuarioDaoImpl) context.getBean("usuarioDao");
+        System.out.print(" -- Load Usario::"+usuario.getUser().size());
+        
+        for(Usuario u: usuario.getUser()){
+        	System.err.println(" -- User: "+u.getNombre()+" email: "+u.getEmail());
+        }
+ 
+    }
+}
