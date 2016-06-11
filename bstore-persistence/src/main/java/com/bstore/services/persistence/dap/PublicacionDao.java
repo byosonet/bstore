ad1485b2a9c8095cc964056bd45f1fb96a0451cd
gtrejo <gtrejo.armenta@gmail.com>
2016-06-10 20:04:26 -0500

    modify mapping publicacion

---
+++ bstore-persistence/src/main/java/com/bstore/services/persistence/dap/PublicacionDao.java
@@ -0,0 +1,7 @@
+package com.bstore.services.persistence.dap;
+
+import com.bstore.services.persistence.pojo.Publicacion;
+
+public interface PublicacionDao {
+	Publicacion getPublicacion(int idPublicacion);
+}
