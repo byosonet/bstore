eb9e0f0c9503e3286e1e52aec6ee9af5ed6d3ebf
gtrejo <byosonet@gmail.com>
2016-07-05 22:52:02 -0500

    add session editorialController

--- bstore-web/src/main/java/com/bstore/services/controller/EditorialController.java
+++ bstore-web/src/main/java/com/bstore/services/controller/EditorialController.java
@@ -1,9 +1,11 @@
 package com.bstore.services.controller;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
@@ -29,60 +31,69 @@ import com.bstore.services.service.EditorialService;
 public class EditorialController {
 
 	private Logger logger = Logger.getLogger(EditorialController.class);
+	private static final String NAME_CONTROLLER="[--EditorialController--]";
 
 	@Autowired
 	private EditorialService editorialService;
 
 	@RequestMapping(value="/getAll",method = RequestMethod.GET)
-	public String getAll(Model model, HttpServletRequest request){
-		logger.info("editorialController.getAll()");
-
-		List<Editorial> editorialList = editorialService.getAll();
-		if(editorialList != null){
-			model.addAttribute("editoriales",editorialList);
+	public String getAll(Model model, HttpServletRequest request, HttpServletResponse response) throws IOException{
+		logger.info("editorialController.getAll(): "+NAME_CONTROLLER+"/getAll");
+		
+		HttpSession session= (HttpSession) request.getSession(false);
+		if(session!=null && session instanceof HttpSession && session.getAttribute("token")!=null){
+			List<Editorial> editorialList = editorialService.getAll();
+			if(editorialList != null){
+				model.addAttribute("editoriales",editorialList);
+			}
+			
+			@SuppressWarnings("unchecked")
+			Map<Coleccion, List<Publicacion>> menu= (Map<Coleccion, List<Publicacion>>) session.getAttribute("menu");
+			 model.addAttribute("menu",menu);
+			logger.info("Recuperando de sesion menu: "+session.getAttribute("menu").toString());
+		}else{
+			response.sendRedirect(request.getContextPath());
 		}
-		
-		HttpSession session= (HttpSession) request.getSession();
-		@SuppressWarnings("unchecked")
-		Map<Coleccion, List<Publicacion>> menu= (Map<Coleccion, List<Publicacion>>) session.getAttribute("menu");
-		 model.addAttribute("menu",menu);
-		logger.info("Recuperando de sesion menu: "+session.getAttribute("menu").toString());
-		
 		return "editoriales";
 	}
 
 	@RequestMapping(value="/add",method = RequestMethod.GET)
-	public String editorialAdd(Model model, HttpServletRequest request){
-		logger.info("editorialController.editorialAdd()");
+	public String editorialAdd(Model model, HttpServletRequest request, HttpServletResponse response) throws IOException{
+		logger.info("editorialController.editorialAdd(): "+NAME_CONTROLLER+"/add");
 		logger.info("---------------------------------------------------------------------------------");
 		
-		HttpSession session= (HttpSession) request.getSession();
-		@SuppressWarnings("unchecked")
-		Map<Coleccion, List<Publicacion>> menu= (Map<Coleccion, List<Publicacion>>) session.getAttribute("menu");
-		 model.addAttribute("menu",menu);
-		logger.info("Recuperando de sesion menu: "+session.getAttribute("menu").toString());
+		HttpSession session= (HttpSession) request.getSession(false);
+		if(session!=null && session instanceof HttpSession && session.getAttribute("token")!=null){
+			@SuppressWarnings("unchecked")
+			Map<Coleccion, List<Publicacion>> menu= (Map<Coleccion, List<Publicacion>>) session.getAttribute("menu");
+			 model.addAttribute("menu",menu);
+			logger.info("Recuperando de sesion menu: "+session.getAttribute("menu").toString());
+		}else{
+			response.sendRedirect(request.getContextPath());
+		}
 		
 		return "editorialAdd";
 	}
 	
 	@RequestMapping(value="/saveEditorial",method = RequestMethod.POST)
-	public String saveEditorial(Model model, HttpServletRequest request, @ModelAttribute("editorialForm") Editorial editorial){
-		logger.info("editorialController.editorialAdd()");
+	public String saveEditorial(Model model, HttpServletRequest request, HttpServletResponse response, 
+			@ModelAttribute("editorialForm") Editorial editorial) throws IOException{
+		logger.info("editorialController.editorialAdd(): "+NAME_CONTROLLER+"/saveEditorial");
 		
-		HttpSession session= (HttpSession) request.getSession();
-		@SuppressWarnings("unchecked")
-		Map<Coleccion, List<Publicacion>> menu= (Map<Coleccion, List<Publicacion>>) session.getAttribute("menu");
-		 model.addAttribute("menu",menu);
-		logger.info("Recuperando de sesion menu: "+session.getAttribute("menu").toString());
-		
-		System.out.println("nombre de la nueva editorial: "+editorial.getNombre());
-		
-		
-		
-		//Para regresar a lista de editoriales
-		List<Editorial> editorialList = editorialService.getAll();
-		if(editorialList != null){
-			model.addAttribute("editoriales",editorialList);
+		HttpSession session= (HttpSession) request.getSession(false);
+		if(session!=null && session instanceof HttpSession && session.getAttribute("token")!=null){
+			@SuppressWarnings("unchecked")
+			Map<Coleccion, List<Publicacion>> menu= (Map<Coleccion, List<Publicacion>>) session.getAttribute("menu");
+			 model.addAttribute("menu",menu);
+			logger.info("Recuperando de sesion menu: "+session.getAttribute("menu").toString());
+			logger.info("nombre de la nueva editorial: "+editorial.getNombre());
+			//Para regresar a lista de editoriales
+			List<Editorial> editorialList = editorialService.getAll();
+			if(editorialList != null){
+				model.addAttribute("editoriales",editorialList);
+			}
+		}else{
+			response.sendRedirect(request.getContextPath());
 		}
 		return "editoriales";
 	}
