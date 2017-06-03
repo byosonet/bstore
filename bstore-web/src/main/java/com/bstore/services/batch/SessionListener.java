397d4d6e22f44eff7c9d2c0c4ce288566eb27171
byosonet <byosonet@gmail.com>
2017-06-02 22:26:39 -0500

    Se agrega variable contador de usuario en linea

--- bstore-web/src/main/java/com/bstore/services/batch/SessionListener.java
+++ bstore-web/src/main/java/com/bstore/services/batch/SessionListener.java
@@ -4,9 +4,6 @@ package com.bstore.services.batch;
  *
  * @author guta
  */
-import java.util.HashMap;
-import java.util.Map;
-import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 import org.apache.log4j.Logger;
@@ -15,18 +12,22 @@ public class SessionListener implements HttpSessionListener {
 
     private final Logger log = Logger.getLogger(SessionListener.class);
 
-    private static Map<String, HttpSession> map = new HashMap<String, HttpSession>();
+    private static int activeSessions = 0;
 
     @Override
     public void sessionCreated(HttpSessionEvent event) {
-        String id = event.getSession().getId();
-        map.put(id, event.getSession());
         this.log.info("Se crea nueva JSESSIONID: " + event.getSession().getId());
+        activeSessions++;
+        this.log.info("Total de usuarios activos en sistema: " + activeSessions);
     }
 
     @Override
     public void sessionDestroyed(HttpSessionEvent event) {
         this.log.info("Se borra JSESSIONID: " + event.getSession().getId());
+        if (activeSessions > 0) {
+            activeSessions--;
+            this.log.info("Total de usuarios activos en sistema: " + activeSessions);
+        }
     }
 
 }
