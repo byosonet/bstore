e0b3f62bc2dc98f2ee19175686ed00efa95e7a28
byosonet <byosonet@gmail.com>
2017-09-04 22:35:57 -0500

    Se elimina el puerto 8080

--- bstore-web/src/main/webapp/resources/js/jquery/blockUI.js
+++ bstore-web/src/main/webapp/resources/js/jquery/blockUI.js
@@ -112,7 +112,7 @@
 		// override these in your code to change the default behavior and style
 		$.blockUI.defaults = {
 			// message displayed when blocking (use null for no message)
-			message:  '<h5 style="text-align:center;margin-bottom:10px" class="alert alert-info"><label>Espere... </label><img style="width: 50px" src="http://50.21.179.206:8080/bstore/static/resources/img/small-loader.gif"></h5>',
+			message:  '<h5 style="text-align:center;margin-bottom:10px" class="alert alert-info"><label>Espere... </label><img style="width: 50px" src="http://50.21.179.206/bstore/static/resources/img/small-loader.gif"></h5>',
 
 			title: '',		// title string; only used when theme == true
 			draggable: true,	// only used when theme == true (requires jquery-ui.js to be loaded)
